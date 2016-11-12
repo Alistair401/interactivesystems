d58a8ba0dba7d2ff33f7b8f0f12d37917d2bf9b9
Alistair Miles <ac.counter@hotmail.com>
2016-11-12 14:38:34 +0000

    Can now get the id of the selected project (WIP)

--- public/projectSelectClient.js
+++ public/projectSelectClient.js
@@ -2,23 +2,32 @@ $(document).ready(function () {
     var socket = io();
     refreshProjects();
     socket.on('list_projects', function (data) {
-        if (data.length > 0){
-            data.forEach(function(item,index){
-                $('#project-select').append('<div value="'+item.id+'" class="btn btn-default btn-block btn-primary project">'+item.title+'<b class="cust-grey-text"> #'+item.id+'</b></div>');
+        if (data.length > 0) {
+            data.forEach(function (item, index) {
+                $('#project-select').append('<div id="' + item.id + '" class="btn btn-default btn-block btn-primary project">' + item.title + '<b class="cust-grey-text"> #' + item.id + '</b></div>');
             });
+        } else {
+            //            $('#project-select').append('<h3>You </h3>')
         }
     });
-    $('#project-submit').click(function(){
-        socket.emit('add_project',{title:$('#project-title').val()});
+    $('#project-submit').click(function () {
+        socket.emit('add_project', {
+            title: $('#project-title').val()
+        });
     });
-    socket.on('add_project_success',function(){
+
+    $('#project-select').on('click', '.project', function () {
+        console.log($(this).attr("id"));
+    });
+
+    socket.on('add_project_success', function () {
         $('#project-modal').modal('hide');
         $('.project').remove();
         refreshProjects();
     });
 
-    function refreshProjects(){
+    function refreshProjects() {
         socket.emit('get_projects');
     }
-});
 
+});
